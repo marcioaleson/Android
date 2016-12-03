4c57d93dd58128f9a297f000075b38af0106b2e1
marcioaleson <marcioaleson@gmail.com>
2016-12-02 23:50:42 -0200

    Notification example.

---
+++ AndroidNotification3/app/src/main/java/br/com/marcioaleson/androidnotification3/NotificationView.java
@@ -0,0 +1,13 @@
+package br.com.marcioaleson.androidnotification3;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationView extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
