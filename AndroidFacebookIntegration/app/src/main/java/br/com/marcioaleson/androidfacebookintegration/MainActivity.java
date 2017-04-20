a4c79aadba3d7f7f329476124fbf6745d1d25190
marcioaleson <marcioaleson@gmail.com>
2017-04-19 22:01:25 -0300

    Android example Intentshare.

--- AndroidFacebookIntegration/app/src/main/java/br/com/marcioaleson/androidfacebookintegration/MainActivity.java
+++ AndroidFacebookIntegration/app/src/main/java/br/com/marcioaleson/androidfacebookintegration/MainActivity.java
@@ -1,13 +1,34 @@
 package br.com.marcioaleson.androidfacebookintegration;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void open(View view) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Hello, from tutorialspoint");
+        startActivity(Intent.createChooser(shareIntent, "Share your thoughts"));
     }
 }
