760124598b18f72ec35b63a04b4df8a9c8314310
marcioaleson <marcioaleson@gmail.com>
2017-08-23 23:39:03 -0300

    Android Load Spinner Example.

--- AndroidSpinnerExample/app/src/main/java/br/com/marcioaleson/androidspinnerexample/MainActivity.java
+++ AndroidSpinnerExample/app/src/main/java/br/com/marcioaleson/androidspinnerexample/MainActivity.java
@@ -1,13 +1,31 @@
 package br.com.marcioaleson.androidspinnerexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = (ProgressBar) findViewById(R.id.progressBar1);
+        spinner.setVisibility(View.GONE);
+    }
+
+    public void load(View view) {
+        spinner.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
     }
 }
