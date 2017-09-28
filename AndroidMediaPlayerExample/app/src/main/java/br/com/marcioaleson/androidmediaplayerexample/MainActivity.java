04a404b15f67992983414000bd395452644a7609
marcioaleson <marcioaleson@gmail.com>
2017-09-27 21:29:05 -0300

    Added mp3 file example.

--- AndroidMediaPlayerExample/app/src/main/java/br/com/marcioaleson/androidmediaplayerexample/MainActivity.java
+++ AndroidMediaPlayerExample/app/src/main/java/br/com/marcioaleson/androidmediaplayerexample/MainActivity.java
@@ -8,8 +8,6 @@ import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -38,7 +36,7 @@ public class MainActivity extends Activity {
         seekbar = (SeekBar) findViewById(R.id.seekBar1);
         playButton = (ImageButton) findViewById(R.id.imageButton1);
         pauseButton = (ImageButton) findViewById(R.id.imageButton2);
-        songName.setText("song.mp3");
+        songName.setText("Symphony Number 9 Beethoven");
         mediaPlayer = MediaPlayer.create(this, R.raw.song);
         seekbar.setClickable(false);
         pauseButton.setEnabled(false);
