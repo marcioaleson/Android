335f5e01caa296a1c6eec493f86b03eecc379dd2
marcioaleson <marcioaleson@gmail.com>
2017-10-25 23:36:22 -0200

    Android Navigation Example.

---
+++ AndroidNavigationExample/app/src/main/java/br/com/marcioaleson/androidnavigationexample/MainActivity2.java
@@ -0,0 +1,44 @@
+package br.com.marcioaleson.androidnavigationexample;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity2 extends Activity {
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+// Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+        MainActivity2.this.finish();
+    }
+}
