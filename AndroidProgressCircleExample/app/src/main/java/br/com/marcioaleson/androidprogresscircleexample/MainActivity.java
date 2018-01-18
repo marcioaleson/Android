dd49237b3b252af6ccc080fdb2ea89757e040967
marcioaleson <marcioaleson@gmail.com>
2018-01-17 23:43:11 -0200

    Adroind Progress Circle Example.

---
+++ AndroidProgressCircleExample/app/src/main/java/br/com/marcioaleson/androidprogresscircleexample/MainActivity.java
@@ -0,0 +1,50 @@
+package br.com.marcioaleson.androidprogresscircleexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    private ProgressDialog progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progress = new ProgressDialog(this);
+    }
+
+    public void open(View view) {
+        progress.setMessage("Downloading Music :) ");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+        progress.show();
+        final int totalProgressTime = 100;
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                int jumpTime = 0;
+                while (jumpTime < totalProgressTime) {
+                    try {
+                        sleep(200);
+                        jumpTime += 5;
+                        progress.setProgress(jumpTime);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
