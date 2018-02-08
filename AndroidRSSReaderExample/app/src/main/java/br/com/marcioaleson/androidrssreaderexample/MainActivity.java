ac08353bf18202e0e60fde64a643304c4dddc6a9
marcioaleson <marcioaleson@gmail.com>
2018-02-07 23:55:55 -0200

    Added MainActivity.class.

--- AndroidRSSReaderExample/app/src/main/java/br/com/marcioaleson/androidrssreaderexample/MainActivity.java
+++ AndroidRSSReaderExample/app/src/main/java/br/com/marcioaleson/androidrssreaderexample/MainActivity.java
@@ -2,12 +2,37 @@ package br.com.marcioaleson.androidrssreaderexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private String finalUrl = "http://tutorialspoint.com/android/sampleXML.xml";
+    private HandleXML obj;
+    private EditText title, link, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        title = (EditText) findViewById(R.id.editText1);
+        link = (EditText) findViewById(R.id.editText2);
+        description = (EditText) findViewById(R.id.editText3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void fetch(View view) {
+        obj = new HandleXML(finalUrl);
+        obj.fetchXML();
+        while (obj.parsingComplete) ;
+        title.setText(obj.getTitle());
+        link.setText(obj.getLink());
+        description.setText(obj.getDescription());
     }
 }
